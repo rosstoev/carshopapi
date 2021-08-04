4d78a4910fd628b30bab0ae329f7828f74aca75d
ros <rossto3v@gmail.com>
2021-08-04 11:10:03 +0300

    Make jwt authentication

---
+++ src/Controller/UserController.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/register/{email}/{password}", name="register")
+     */
+    public function register(string $email, string $password, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles(['ROLE_USER']);
+        $hashPassword = $passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashPassword);
+        $em->persist($user);
+        $em->flush();
+
+        return new Response(sprintf("Successfully created new User: %s", $user->getUserIdentifier()));
+
+    }
+
+    /**
+     * @Route ("/token/{email}/{password}", name="token")
+     */
+    public function getToken(string $email, string $password, UserRepository $userRepo, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $jwtManger)
+    {
+        $user = $userRepo->findOneBy(['email' => $email]);
+        $valid = false;
+        if (!empty($user)) {
+            $valid = $passwordHasher->isPasswordValid($user, $password);
+        }
+
+        if ($valid == true) {
+            $token = $jwtManger->create($user);
+            $message = sprintf("The token is: %s", $token);
+        } else {
+            $message = "Unknown user";
+        }
+
+        return new Response($message);
+    }
+}
---
+++ src/Security/JwtTokenAuthenticator.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\AuthorizationHeaderTokenExtractor;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+
+class JwtTokenAuthenticator extends AbstractGuardAuthenticator
+{
+
+    private JWTEncoderInterface $encoder;
+    private EntityManagerInterface $em;
+    private UserRepository $userRepo;
+
+    public function __construct(JWTEncoderInterface $encoder, EntityManagerInterface $em, UserRepository $userRepo)
+    {
+        $this->encoder = $encoder;
+        $this->em = $em;
+        $this->userRepo = $userRepo;
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $data = [
+            // you might translate this message
+            'message' => 'Authentication Required'
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function supports(Request $request)
+    {
+        return $request->headers->get('Authorization') ?? false;
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $extractor = new AuthorizationHeaderTokenExtractor('Bearer', 'Authorization');
+
+        $token = $extractor->extract($request);
+
+        if (!$token) {
+            return;
+        }
+
+        return $token;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        if (empty($credentials)) {
+            throw new CustomUserMessageAuthenticationException('Invalid Token');
+        }
+
+        $data = $this->encoder->decode($credentials);
+
+        if ($data == false) {
+            throw new CustomUserMessageAuthenticationException('Invalid Token');
+        }
+
+        $email = $data['username'];
+
+        return $this->userRepo->findOneBy(['email' => $email]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        // TODO: Implement onAuthenticationSuccess() method.
+    }
+
+    public function supportsRememberMe()
+    {
+       return false;
+    }
+}
